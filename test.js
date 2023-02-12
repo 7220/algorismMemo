9d7aaf7cbeb7ba33d257e0262f26db8031cfcb80
7220 <kumasaki@naver.com>
2023-02-12 14:28:10 +0900

    New Question and README

--- test.js
+++ test.js
@@ -1,11 +1,19 @@
-function solution(my_string, n) {
-  const newString = []
+function solution(my_string) {
+  const list = []
   for (let i = 0; i < my_string.length; i++) {
-      for (let j = 0; j < n; j++) {
-          newString.push(my_string[i])
-
-      }
+      const c = my_string.charCodeAt(i)
+      if (c >= 48 && c <= 57) list.push(String.fromCharCode(c))
   }
-  return newString.join('')
+  for (let i = 0; i < list.length; i++) {
+    for (let j = 0; j < list.length; j++) {
+      if (list[j] > list[j + 1]) {
+      const a = list[j]
+      list[j] = list[j + 1]
+      list[j + 1] = a
+      }
+    }
+  }
+  const result = list.map(x => parseInt(x))
+  return result
 }
-console.log(solution('hello', 5))
+console.log(solution("hi1240897124"))
