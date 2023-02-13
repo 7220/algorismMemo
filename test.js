058fce5fe3e06cca9a83bdce6eaf96e2678392ce
7220 <kumasaki@naver.com>
2023-02-13 14:36:49 +0900

    function & memo add

--- test.js
+++ test.js
@@ -1,19 +1,13 @@
-function solution(my_string) {
+function solution(num, k) {
+  let n = String(num)
   const list = []
-  for (let i = 0; i < my_string.length; i++) {
-      const c = my_string.charCodeAt(i)
-      if (c >= 48 && c <= 57) list.push(String.fromCharCode(c))
+  for (let i = 0; i < n.length; i++) {
+      list.push(n[i])
   }
   for (let i = 0; i < list.length; i++) {
-    for (let j = 0; j < list.length; j++) {
-      if (list[j] > list[j + 1]) {
-      const a = list[j]
-      list[j] = list[j + 1]
-      list[j + 1] = a
-      }
-    }
+      if (list[i] === k) return i + 1
   }
-  const result = list.map(x => parseInt(x))
-  return result
+  console.log(list)
+  return -1
 }
-console.log(solution("hi1240897124"))
+console.log(solution(98273, 3))
