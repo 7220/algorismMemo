a19b7d825629f30ec8f57b95991270f021c5bb30
7220 <kumasaki@naver.com>
2023-02-14 16:04:07 +0900

    function function

--- test.js
+++ test.js
@@ -1,13 +1,5 @@
-function solution(num, k) {
-  let n = String(num)
-  const list = []
-  for (let i = 0; i < n.length; i++) {
-      list.push(n[i])
-  }
-  for (let i = 0; i < list.length; i++) {
-      if (list[i] === k) return i + 1
-  }
-  console.log(list)
-  return -1
+function solution(age) {
+  let str = 'abcdefghij'
+  return Array.from(String(age)).map(a => str[a]).join('')
 }
-console.log(solution(98273, 3))
+console.log(solution(100))
