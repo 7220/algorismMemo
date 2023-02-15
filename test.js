c23d6f887e26e92743aa1e89f3d587dacf4c187e
7220 <kumasaki@naver.com>
2023-02-15 20:27:09 +0900

    Function add

--- test.js
+++ test.js
@@ -1,5 +1,55 @@
-function solution(age) {
-  let str = 'abcdefghij'
-  return Array.from(String(age)).map(a => str[a]).join('')
+function solution(emergency) {
+    const s = so(emergency)
+    const d = m(s)
+    return d
 }
-console.log(solution(100))
+
+function so(p) {
+    for (let i = 0; i < p.length; i++) {
+        for (let j = 0; j < p.length - i - 1; j++) {
+            if (p[j] < p[j + 1]) {
+                let a = p[j]
+                p[j] = p[j + 1]
+                p[j + 1] = a
+            }
+        }
+    }
+    return p
+}
+
+function m(s) {
+    const list = []
+    for (let i = 0; i < s.length; i++) {
+        list.push(s.indexOf(s[i]) + 1)
+    }
+    return list
+}
+
+function solution2(emergency) {
+    let indices = [];
+    for (let i = 0; i < emergency.length; i++) {
+        indices.push(i);
+    }
+    console.log(indices)
+
+    indices.sort((a, b) => emergency[b] - emergency[a])
+    console.log(indices)
+
+    let result = [];
+    for (let i = 0; i < indices.length; i++) {
+        result.push(indices.indexOf(i) + 1);
+    }
+
+    return result;
+}
+
+let emergency = [30, 10, 23, 6, 100];
+console.log(solution2(emergency));
+
+function asd(p) {
+    const list = []
+    for (let i = 0; i < p.length; i++) {
+        list.push(p[i] + p[i])
+    }
+    return list
+}
