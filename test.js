6c4a8b413b6435bbae1252cc2f6671ce52de6063
7220 <kumasaki@naver.com>
2023-02-16 21:46:23 +0900

    Function add

--- test.js
+++ test.js
@@ -1,55 +1,15 @@
-function solution(emergency) {
-    const s = so(emergency)
-    const d = m(s)
-    return d
+function combination(n, r) {
+  let numerator = 1;
+  for (let i = n; i > n - r; i--) {
+    numerator *= i;
+  }
+  let denominator = 1;
+  for (let i = 1; i <= r; i++) {
+    denominator *= i;
+  }
+  return numerator / denominator;
 }
 
-function so(p) {
-    for (let i = 0; i < p.length; i++) {
-        for (let j = 0; j < p.length - i - 1; j++) {
-            if (p[j] < p[j + 1]) {
-                let a = p[j]
-                p[j] = p[j + 1]
-                p[j + 1] = a
-            }
-        }
-    }
-    return p
-}
-
-function m(s) {
-    const list = []
-    for (let i = 0; i < s.length; i++) {
-        list.push(s.indexOf(s[i]) + 1)
-    }
-    return list
-}
-
-function solution2(emergency) {
-    let indices = [];
-    for (let i = 0; i < emergency.length; i++) {
-        indices.push(i);
-    }
-    console.log(indices)
-
-    indices.sort((a, b) => emergency[b] - emergency[a])
-    console.log(indices)
-
-    let result = [];
-    for (let i = 0; i < indices.length; i++) {
-        result.push(indices.indexOf(i) + 1);
-    }
-
-    return result;
-}
-
-let emergency = [30, 10, 23, 6, 100];
-console.log(solution2(emergency));
-
-function asd(p) {
-    const list = []
-    for (let i = 0; i < p.length; i++) {
-        list.push(p[i] + p[i])
-    }
-    return list
+function solution(balls, share) {
+  return combination(balls, share);
 }
