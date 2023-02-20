90432ca5e31c118e70b1fe4c2fbdbc15ac8511d5
7220 <kumasaki@naver.com>
2023-02-21 00:06:29 +0900

    function add & level 0 complete

--- test.js
+++ test.js
@@ -1,19 +1,6 @@
-function solution(chicken) {
-  let order = 0;
-  let coupon = 0
-  let bonusChicken = 0
-
-  for (let i = 1; i <= chicken; i++ ) {
-    order++
-    coupon++
-    if (coupon !== 0 && coupon % 10 === 0) {
-      coupon++
-      bonusChicken++
-    }
-  }
-  return bonusChicken
+function solution(n) {
+  return String(n).split('').reduce((acc, cur) => Number(acc) + Number(cur), 0)
 }
+console.log(solution(10928102))
 
-
-console.log(solution(1081))
-
+console.log(10928102+"")
