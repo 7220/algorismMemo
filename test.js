36dbdff0b4eb0c7db239f6098ceac1f6ff01129a
7220 <kumasaki@naver.com>
2023-02-22 04:24:41 +0900

    function add & level 1 start

--- test.js
+++ test.js
@@ -1,6 +1,28 @@
-function solution(n) {
-  return String(n).split('').reduce((acc, cur) => Number(acc) + Number(cur), 0)
+function solution(s) {
+  if (s.length !== 4 && s.length !== 6) return false
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(' ') >= 0) return false
+    if (s.charCodeAt(i) >= 65 && s.charCodeAt(i) <= 90) return false
+    if (s.charCodeAt(i) >= 97 && s.charCodeAt(i) <= 122) return false
+    if (s.charCodeAt(i) >= 48 && s.charCodeAt(i) <= 57) return true 
+  }
+  return false
 }
-console.log(solution(10928102))
 
-console.log(10928102+"")
+function solution2(s) {
+  const a = 'abcdefghijklmnopqrstuvwxyz '
+  const A = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ '
+  if (s.length !== 4 && s.length !== 6) return false
+  for (let i = 0; i < a.length; i++) {
+    if (s.indexOf(a[i])) return false
+    if (s.indexOf(A[i])) return false
+  }
+  return true
+}
+
+console.log(solution2("1235"))
+
+
+
+
+
