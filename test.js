538f55dfb355031d9814f5dc0cbb28b9452f4a70
7220 <kumasaki@naver.com>
2023-02-23 01:21:27 +0900

    function add & level 1/3 complete

--- test.js
+++ test.js
@@ -1,26 +1,14 @@
-function solution(s) {
-  if (s.length !== 4 && s.length !== 6) return false
-  for (let i = 0; i < s.length; i++) {
-    if (s.indexOf(' ') >= 0) return false
-    if (s.charCodeAt(i) >= 65 && s.charCodeAt(i) <= 90) return false
-    if (s.charCodeAt(i) >= 97 && s.charCodeAt(i) <= 122) return false
-    if (s.charCodeAt(i) >= 48 && s.charCodeAt(i) <= 57) return true 
+function solution(arr1, aar2) {
+  const list = []
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr1[0].length; j++) {
+      list[i][j] = arr1[i][j] + arr2[i][j]
+    } 
   }
-  return false
+  return list
 }
 
-function solution2(s) {
-  const a = 'abcdefghijklmnopqrstuvwxyz '
-  const A = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ '
-  if (s.length !== 4 && s.length !== 6) return false
-  for (let i = 0; i < a.length; i++) {
-    if (s.indexOf(a[i])) return false
-    if (s.indexOf(A[i])) return false
-  }
-  return true
-}
-
-console.log(solution2("1235"))
+console.log(solution([[1,2],[2,3]], [[3,4],[5,6]]))
 
 
 
