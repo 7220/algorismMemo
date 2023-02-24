9e4d227480aecb318983e46326b5f60b209068dc
7220 <kumasaki@naver.com>
2023-02-24 18:15:48 +0900

    function add

--- test.js
+++ test.js
@@ -1,14 +1,9 @@
-function solution(arr1, aar2) {
-  const list = []
-  for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr1[0].length; j++) {
-      list[i][j] = arr1[i][j] + arr2[i][j]
-    } 
-  }
-  return list
+function solution(s) {
+  const string = s.split(' ')
+  console.log(string)
 }
 
-console.log(solution([[1,2],[2,3]], [[3,4],[5,6]]))
+console.log(solution("try hello world"))
 
 
 
