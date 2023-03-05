00a8dee6ea8925b1ae8bcd1ff9ac32b7b1f1fab5
7220 <kumasaki@naver.com>
2023-03-06 00:05:05 +0900

    level 1 Research

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
-function solution(dartResult) {
-  const list = dartResult.split('')
-  console.log(list)
+function solution(n, lost, reserve) {
+
 }
 
-
-console.log(solution("1S2D*3T"))
+console.log(solution())
