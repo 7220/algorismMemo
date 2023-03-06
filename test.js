bcdc3d0bc07609507f6608cdce23a3b4fdb926de
7220 <kumasaki@naver.com>
2023-03-07 00:54:18 +0900

    level 2 start

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
-function solution(n, lost, reserve) {
-
+function solution(s) {
+  let rotation = 0
+  let count = 0
+  while (s !== "1") {
+    const a = s.replace(/0/g, "")
+    count += s.length - a.length
+    s = (a.length).toString(2)
+    rotation++
+  }
+  return [rotation, count]
 }
 
-console.log(solution())
+console.log(solution(15))
