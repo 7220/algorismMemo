c388e90c641ce21d8259d371625dd354db7def15
7220 <kumasaki@naver.com>
2023-03-09 02:37:19 +0900

    level 3 start

--- test.js
+++ test.js
@@ -1,13 +1,7 @@
-function solution(s) {
-  let rotation = 0
-  let count = 0
-  while (s !== "1") {
-    const a = s.replace(/0/g, "")
-    count += s.length - a.length
-    s = (a.length).toString(2)
-    rotation++
-  }
-  return [rotation, count]
+function solution(n, words) {
+
+  
 }
 
-console.log(solution(15))
+
+console.log(solution("baabaa"))
