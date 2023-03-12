a91c64d9fb42106891a348e359875dbed876c16b
7220 <kumasaki@naver.com>
2023-03-13 04:19:18 +0900

    level 2 add

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
-function solution(k, tangerine) {
-  
+function solution(n, left, right) {
+  const result = []
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      const n = Math.max(i, j) + 1
+      if (left <= j && j <= right) result.push(n)
+    }
+  } 
+  return result
 }
 
-
+console.log(solution(3, 2, 5))
