4f541e6e94486c929458849ea7e5aa0460113a53
7220 <kumasaki@naver.com>
2023-03-16 02:28:10 +0900

    level 4 add

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
 function solution(numbers) {
-  const str = numbers.map(n => String(n))
-  const sortedStr = str.sort((a, b) => (b + a) - (a + b))
-  const result = sortedStr.join('')
-  return result[0] === '0' ? '0' : result 
+
 }
 
 
